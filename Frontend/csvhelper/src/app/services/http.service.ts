85d2516a0ab379424cedb1a3f087e0ecb6f9361d
PATI <patrickmistlberger@gmail.com>
2019-08-19 13:54:18 +0200

    finish
    finish maybe front end little bugs

--- Frontend/csvhelper/src/app/services/http.service.ts
+++ Frontend/csvhelper/src/app/services/http.service.ts
@@ -5,18 +5,19 @@ import { Injectable } from '@angular/core';
   providedIn: 'root'
 })
 export class HttpService {
-  my = 'http://localhost:57713/api/modeltexts';
+  my = 'https://clearingcenter.sport2000.at/PutModelTextApi';
+  first = 'http://localhost:57713/api/modelText';
   constructor() { }
 
   async sendModelTextData(data) {
     // async fetching works at post response come in with status
-    await fetch(this.my, {
+    await fetch(this.first, {
       method: 'POST',
       body: JSON.stringify(data),                           // do data convert to Json
       headers: {
         'Content-Type': 'application/json'
       }
-    }).then(res => res.status === 200 ? alert('Gespeichert!!!!!') : alert('Nicht Gespeichert!!!!!')).catch((error) => console.log(error));
+    }).then(res => res.status === 200 ? alert('Gespeichert!!!!!') : null).catch((error) => alert(error));
     // if it fail
   }
 }
